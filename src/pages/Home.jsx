5d51e2c19bc09cb1cc8513a760f1031a729927df
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-09-12 13:41:09 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden" >
+      {/* Toggle Effect  */}
+      <ThemeToggle />
+
+      {/* Background Effects  */}
+
+      {/* Navbar  */}
+
+      {/* Main Content */}
+      
+    
+      
+      </div>
+  )
+}
